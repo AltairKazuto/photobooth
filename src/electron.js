ee9eb4b07f7032775ae2a910f80cea1660e21d3b
AltairKazuto <ryvbiensent@gmail.com>
2025-09-03 17:23:25 +0800

    fix

---
+++ src/electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // devTools: false, // Disable DevTools
+            nodeIntegration: true, // Enable Node.js integration in the renderer process
+            contextIsolation: false // Disable context isolation for simplicity in development
+        },
+    });
+
+    win.loadURL('http://localhost:3000'); // Load your React app's development server
+    win.webContents.openDevTools(); // Open DevTools for debugging
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
